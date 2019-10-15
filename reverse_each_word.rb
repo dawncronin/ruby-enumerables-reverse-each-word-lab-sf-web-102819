e8cf0704216894e65355044a0da93b980f5b03fa
Dawn Cronin <dawncronin7@gmail.com>
2019-10-15 02:19:57 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -4,7 +4,7 @@ def reverse_each_word(string)
   new_arr = []
   arr.each do |word|
     reversed = ""
-    word.each_with_char do |ele|
+    word.each_char do |ele|
       reversed = ele + reversed
     end
     new_arr << reversed
