893ac25914598726f165d967cb8034b3b65a01f5
Dawn Cronin <dawncronin7@gmail.com>
2019-10-15 02:17:22 +0000

    Automatically backed up by Learn

---
+++ reverse_each_word.rb
@@ -0,0 +1,12 @@
+def reverse_each_word(string)
+  
+  arr.split(" ")
+  new_arr << word
+  arr.each do |word|
+    reversed = ""
+    word.each_with_char do |ele|
+      reversed = ele + reversed
+    end
+    new_arr << 
+    
+      
